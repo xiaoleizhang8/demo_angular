fbc0491539afd97561e8f18d08e7fda018d723fe
xiaoleizhang8 <1783421370@qq.com>
2014-03-04 15:17:36 +0800

    init_project

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Server::Application.config.secret_key_base = '7897e597346808ac241c403b7df99f3520e7d49028f739ff7ae456e281a0489cf30482e96e9f100c8ad18a2c0fcd7441a29f09c6991509a20d8fb427baa784f8'
---
+++ www/js/routes.js
@@ -0,0 +1,24 @@
+myModule.config(function($routeProvider) {
+
+
+    //routing generate
+
+    //manual routing
+});
+
+/** Here is example
+myModule.config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "pages/activity_list_page.html",
+        controller: ActivityListController
+    }).when("/activity/create", {
+            templateUrl: "pages/activity_create_page.html",
+            controller: ActivityCreateController
+        }).when("/sign_ups/list/:activity_name", {
+            templateUrl: "pages/apply_page.html",
+            controller: SignUpListController
+        }).otherwise({
+            redirectTo: "/"
+        });
+});
+**/
